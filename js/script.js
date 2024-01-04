e789dd8ed75bce1cc98a59fb65b1a0226a6f7ff2
unknown <muhammadawais8091@gmail.com>
2024-01-04 23:00:21 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+var accordions = document.getElementsByClassName("accordion");
+ 
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function() {
+    this.classList.toggle('is-open');
+    
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      // accordion is currently open, so close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is currently closed, so open it
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
